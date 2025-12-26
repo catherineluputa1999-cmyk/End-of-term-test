fd6f6da35c55d7b9db77faf2273bff2fbfe6241d
catherineluputa1999-cmyk <catherineluputa1999@gmail.com>
2025-12-26 22:54:36 +0000

    Add files via upload

---
+++ ict113_variables.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+ int main(){
+
+ char name[] = "Catherine";
+
+ int age = 20;
+  
+ float gpa = 3.8;
+
+ printf("Name: %s\n",name);
+
+ printf("Age: %d\n",age);
+
+ printf("GPA: %.1f\n",gpa);
+
+ 
+
+ return 0;
+}
